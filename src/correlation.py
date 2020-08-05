76f146d9058b32a48c61f90c911baa65d14acf32
jiangqn <1935448858@qq.com>
2020-08-06 01:40:58 +0800

    Update correlation.py

--- src/correlation.py
+++ src/correlation.py
@@ -8,14 +8,14 @@ def correlation(config):
     # sample_save_path = 'data/sample10000.tsv'
     encoding_save_path = '.'.join(sample_save_path.split('.')[0:-1]) + '.npy'
 
-    df = pd.read_csv(sample_save_path, delimiter='\t')
-    sentiment = np.asarray(df['sentiment']).astype(np.float32)[:, np.newaxis]
-    ppl = np.asarray(df['ppl']).astype(np.float32)[:, np.newaxis]
-    length = np.asarray(df['length']).astype(np.float32)[:, np.newaxis]
     encoding = np.load(encoding_save_path)
+    df = pd.read_csv(sample_save_path, delimiter='\t')
 
-    for name, prop in zip(['sentiment', 'ppl', 'length'], [sentiment, ppl, length]):
+    prop_names = df.columns.values[2:]
+
+    for prop_name in prop_names:
         cca = CCA(n_components=1)
+        prop = np.asarray(df[prop_name]).astype(np.float32)[:, np.newaxis]
         u, v = cca.fit_transform(encoding, prop)
         corr = abs(float(np.corrcoef(u[:, 0], v)[0, 1]))
-        print('%s correlation: %.4f' % (name, corr))
+        print('%s correlation: %.4f' % (prop_name, corr))
