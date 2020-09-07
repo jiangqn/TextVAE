805401ed37228f424aac386cd186377e8ed93732
jiangqn <1935448858@qq.com>
2020-09-07 16:56:49 +0800

    update

--- src/utils/visualize.py
+++ src/utils/visualize.py
@@ -1,5 +1,6 @@
 import torch
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
 import pandas as pd
@@ -21,6 +22,9 @@ def evaluate_disentanglement(principal_directions):
             cosine = float(principal_directions[key1].dot(principal_directions[key2]))
             print('%.2f' % cosine, end='\t\t' if j < n_keys - 1 else '\n')
 
+def upperfirst(s: str):
+    return s[0].upper() + s[1:]
+
 def numerical_visualize(base_path: str, encoding: np.ndarray, df: pd.DataFrame) -> None:
 
     principal_directions_save_path = os.path.join(base_path, 'principal_directions.pkl')
@@ -33,11 +37,17 @@ def numerical_visualize(base_path: str, encoding: np.ndarray, df: pd.DataFrame) 
 
     cmap = {
         'length': 'viridis',
-        'depth': 'gnuplot'
+        'depth': 'rainbow'
+    }
+    ticks_map = {
+        'length': [0, 3, 6, 9, 12, 15],
+        'depth': [0, 2, 4, 6, 8, 10]
     }
 
     pdf = PdfPages(os.path.join(base_path, 'numerical_visualize.pdf'))
     plt.figure()
+
+
 
     for i, prop in enumerate(['length', 'depth']):
 
@@ -53,16 +63,18 @@ def numerical_visualize(base_path: str, encoding: np.ndarray, df: pd.DataFrame) 
         plt.scatter(x, y, c=feature, s=0.1, cmap=cmap[prop])
 
         corr = np.corrcoef(x, feature)[0, 1]
-        plt.title('%s (correlation: %.4f)' % (prop, corr))
-        plt.colorbar()
+        plt.title('%s (correlation: %.4f)' % (upperfirst(prop), corr))
+        cbar = plt.colorbar()
+        cbar.set_ticks(ticks_map[prop])
 
         pca = PCA(n_components=2)
         pca_encoding = pca.fit_transform(encoding)
 
         plt.subplot(2, 2, i * 2 + 2)
         plt.scatter(pca_encoding[:, 0], pca_encoding[:, 1], c=feature, s=0.1, cmap=cmap[prop])
-        plt.title('%s (PCA)' % prop)
-        plt.colorbar()
+        plt.title('%s (PCA)' % upperfirst(prop))
+        cbar = plt.colorbar()
+        cbar.set_ticks(ticks_map[prop])
 
     plt.subplots_adjust(hspace=0.3)
     pdf.savefig()
@@ -78,6 +90,14 @@ def categorical_visualize(base_path: str, encoding: np.ndarray, df: pd.DataFrame
     cmap = {
         'sentiment': 'viridis',
         'topic': 'Dark2'
+    }
+    cmap_obj = {
+        'sentiment': plt.cm.viridis,
+        'topic': plt.cm.Dark2
+    }
+    category_sets = {
+        'sentiment': ['negative', 'positive'],
+        'topic': ['Clothing_Shoes_and_Jewelry', 'Health_and_Personal_Care']
     }
 
     classifier_path = os.path.join(base_path, 'classifier.pkl')
@@ -107,7 +127,7 @@ def categorical_visualize(base_path: str, encoding: np.ndarray, df: pd.DataFrame
     y = encoding.dot(u)
 
     pdf = PdfPages(os.path.join(base_path, 'categorical_visualize.pdf'))
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(10, 4.5))
 
     plt.subplot(1, 2, 1)
 
@@ -120,14 +140,25 @@ def categorical_visualize(base_path: str, encoding: np.ndarray, df: pd.DataFrame
     intervals = 1000
     plt.scatter(rg(wy / wx * 4, -wy / wx * 4, intervals), rg(4, -4, intervals), c='black', s=0.1)
 
-    plt.title('%s' % prop)
+    custom_lines = [Line2D([0], [0], color=cmap_obj[prop](0.0), lw=2),
+                    Line2D([0], [0], color=cmap_obj[prop](1.0), lw=2)]
+
+    plt.legend(custom_lines, category_sets[prop])
+
+    plt.title('%s' % upperfirst(prop))
 
     pca = PCA(n_components=2)
     pca_encoding = pca.fit_transform(encoding)
 
     plt.subplot(1, 2, 2)
     plt.scatter(pca_encoding[:, 0], pca_encoding[:, 1], c=label, s=0.1, cmap=cmap[prop])
-    plt.title('%s (PCA)' % prop)
+
+    custom_lines = [Line2D([0], [0], color=cmap_obj[prop](0.0), lw=2),
+                    Line2D([0], [0], color=cmap_obj[prop](1.0), lw=2)]
+
+    plt.legend(custom_lines, category_sets[prop])
+
+    plt.title('%s (PCA)' % upperfirst(prop))
 
     pdf.savefig()
     # plt.savefig(os.path.join(base_path, 'categorical_visualize.png'))
@@ -142,11 +173,8 @@ def visualize(config: dict) -> None:
     vanilla_sample_num = config['vanilla_sample']['sample_num']
     vanilla_sample_save_path = os.path.join(base_path, 'vanilla_sample_%d.tsv' % vanilla_sample_num)
     encoding_save_path = '.'.join(vanilla_sample_save_path.split('.')[0:-1]) + '.npy'
-    classifier_path = os.path.join(base_path, 'classifier.pkl')
-
 
     encoding = np.load(encoding_save_path)
-    num, encoding_size = encoding.shape
 
     df = pd.read_csv(vanilla_sample_save_path, delimiter='\t')
 
@@ -156,57 +184,3 @@ def visualize(config: dict) -> None:
 
     categorical_visualize(base_path, encoding, df)
     numerical_visualize(base_path, encoding, df)
-
-    # classifier = joblib.load(classifier_path)
-    # print(classifier.linear.weight.shape)
-    #
-    # prop_names = df.columns.values[2:]
-    #
-    # principal_directions = {}
-    #
-    # pdf = PdfPages(os.path.join(base_path, 'visualize.pdf'))
-    #
-    # plt.figure()
-
-    # for i, prop_name in enumerate(prop_names):
-    #
-    #     plt.subplot(2, 2, i + 1)
-    #
-    #     prop = np.asarray(df[prop_name]).astype(np.float32)[:, np.newaxis]
-    #
-    #     cca = CCA(n_components=1)
-    #     c_encoding, c_prop = cca.fit_transform(encoding, prop)
-    #     v = cca.x_rotations_
-    #
-    #     corr = np.corrcoef(c_encoding[:, 0], c_prop)[0, 1]
-    #
-    #     A = encoding.transpose().dot(encoding) / num
-    #     u = solve(torch.tensor(A).float(), torch.tensor(v).float()).cpu().numpy().astype(np.float64)
-    #
-    #     sign = 1 if corr >= 0 else -1
-    #     v = v * sign
-    #
-    #     principal_directions[prop_name] = v[:, 0]
-    #
-    #     print('%s correlation: %.4f' % (prop_name, sign * corr))
-    #     angle = (np.arccos(np.abs(v[:, 0])) / np.pi * 180).min()
-    #     print('angle: %.4f' % angle)
-    #
-    #     x = encoding.dot(v)[:, 0]
-    #     y = encoding.dot(u)[:, 0]
-    #
-    #     plt.scatter(x, y, c=prop, s=0.1)
-    #     plt.colorbar()
-    #     plt.title('%s (correlation: %.4f)' % (prop_name, abs(corr)))
-    #
-    # evaluate_disentanglement(principal_directions)
-
-    # with open(principal_directions_save_path, 'wb') as handle:
-    #     pickle.dump(principal_directions, handle)
-
-    # plt.subplots_adjust(hspace=0.3)
-    # # save_path = os.path.join(base_path, 'visualize.jpg')
-    # # plt.savefig(save_path)
-    # pdf.savefig()
-    # plt.close()
-    # pdf.close()
