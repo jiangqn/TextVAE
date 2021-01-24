c2a86b6f16d5d20f740595eb66de0de584a83290
jiangqn <1935448858@qq.com>
2021-01-24 20:19:40 +0800

    update

---
+++ run_yelp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+gpu=0
+export CUDA_VISIBLE_DEVICES=$gpu
+echo "register_aggregated_posterior"
+python main.py --model text_vae --task register_aggregated_posterior --config yelp_config.yaml --gpu $gpu
+echo "vanilla_sample"
+python main.py --model text_vae --task vanilla_sample --config yelp_config.yaml --gpu $gpu
+echo "get_features"
+python main.py --model text_vae --task get_features --config yelp_config.yaml --gpu $gpu
+echo "category_analyze"
+python main.py --model text_vae --task category_analyze --config yelp_config.yaml --gpu $gpu
+echo "length_analyze"
+python main.py --model text_vae --task length_analyze --config yelp_config.yaml --gpu $gpu
+echo "depth_analyze"
+python main.py --model text_vae --task depth_analyze --config yelp_config.yaml --gpu $gpu
+echo "linear_categorical_sample"
+python main.py --model text_vae --task linear_categorical_sample --config yelp_config.yaml --gpu $gpu
+echo "linear_length_sample"
+python main.py --model text_vae --task linear_length_sample --config yelp_config.yaml --gpu $gpu
+echo "linear_depth_sample"
+python main.py --model text_vae --task linear_depth_sample --config yelp_config.yaml --gpu $gpu
